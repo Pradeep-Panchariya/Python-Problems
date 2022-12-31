1876685a7df958b1d200b173a931588835f6f789
Pradeep Panchariya <>
2022-12-31 12:31:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+"""
+Author : Pradeep Panchariya
+Email : panchariya11@gmail.com
+"""
+
+def find_prime_factor(num):
+    """
+    find the prime factor for the given number.
+    num : input
+    return : list of prime factor numbers
+    """
+    factors = []
+    i = 2
+    while num!=1:
+        if num%i==0:
+            factors.append(i)
+            num//=i
+        else:
+            i+=1
+    return factors
+
+
+print(find_prime_factor(10))
