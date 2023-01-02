3e737423be46b3f0e9ba931da81e569ad40fc327
Pradeep Panchariya <panchariya11@gmail.com>
2023-01-02 08:57:02 +0000

    updated 2 method for sort_words function

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ Author : Pradeep Panchariya
 Email : panchariya11@gmail.com
 """
 
-
-def sort_words(str):
+def sort_words(string):
     """
     sort the words in the given input string
     str : input of string type
     return : sorted output
-    """
-    # Method -1
+    """ 
+
+    # Method -1 
     return ' '.join(sorted(string.split(), key=str.casefold))
 
-    # # Method -2
+    # # Method -2 
     # sorting_str = sorted([w.lower()+w for w in str.split()])
     # return ' '.join([w[len(w)//2:] for w in sorting_str])
 
-
+    
 print(sort_words("string of WORDS"))
